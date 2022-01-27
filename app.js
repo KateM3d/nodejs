58aa7aded0c4f7cedf923b1af5bcba489cd4e506
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-27 12:59:57 -0500

    fs async method

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs");
 
-// readFileSync -method for reading text files. takes 2 arguments path and format
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
+// for async method we need to provide a callback
 
-console.log(first, second);
-
-// writeFileSync -method for creating text files. takes 2 parameters path and inner. if the file is already excists node will overrite it with the new value
-
-writeFileSync(
-    "./content/result-sync.txt",
-    `Here is the result ${first}, ${second} and here you are`, { flag: "a" }
-);
+readFile("./content/first.txt", "utf8", (err, result) => {
+    if (err) {
+        console.log(err);
+        return null;
+    }
+    const first = result;
+    readFile("./content/second.txt", "utf8", (err, result) => {
+        if (err) {
+            console.log(err);
+            return null;
+        }
+        const second = result;
+        writeFile(
+            "./content/result-async.txt",
+            `here is the result: ${first} and ${second}`, { flag: "a" },
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+            }
+        );
+    });
+});
