2750791b2da4b723fbcad196058f9bef70cd4e43
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-29 15:07:15 -0500

    practicing readFile, writeFirle fs modules with async await

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const http = require("http");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.end("Home Page");
-    } else if (req.url === "/about") {
-        // blocking code
-        for (let i = 0; i < 100; i++) {
-            for (let j = 0; j < 100; j++) {
-                console.log(`${i} ${j}`);
-            }
-        }
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, "utf8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
 
-        res.end("About Page");
-    } else res.end("Error Page");
-});
+// getText("./content/first.txt")
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
 
-server.listen(5000, () => {
-    console.log("server is listening on port 5000...");
-});
+const start = async() => {
+    try {
+        const first = await readFilePromise("./content/first.txt", "utf8");
+        const second = await readFilePromise("./content/second.txt", "utf8");
+        await writeFilePromise(
+            "./content/result-mind-grenade.txt",
+            `file created:${first}, ${second}`
+        );
+
+        console.log(first);
+        console.log(second);
+    } catch (err) {
+        console.log(err);
+    }
+};
+start();
